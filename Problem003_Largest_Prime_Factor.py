f45762b089f953d634c6416d724adf37c1f414fe
M.N <56799686+moussatat@users.noreply.github.com>
2019-11-29 21:14:10 +0100

    Update Problem003_Largest_Prime_Factor.py

--- Problem003_Largest_Prime_Factor.py
+++ Problem003_Largest_Prime_Factor.py
@@ -2,6 +2,7 @@
 # Problem 003 Largest prime factor
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
+# No need to check for prime, we are getting smalled factor that is different from 1. and that is always a prime number.
 import time
 start = time.time()
 
