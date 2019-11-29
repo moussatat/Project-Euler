cb97b8b1032af9763917eea529c7ec4f0f8dcb7d
M.N <56799686+moussatat@users.noreply.github.com>
2019-11-29 21:13:35 +0100

    Update Problem003_Largest_Prime_Factor.py

--- Problem003_Largest_Prime_Factor.py
+++ Problem003_Largest_Prime_Factor.py
@@ -3,28 +3,19 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 import time
-  
-def is_prime(nbr) :
-    i = 2 
-    while i <= nbr and nbr%i !=0:
-        i = i + 1
-    if i == nbr   :
-        return True
-    else :
-        return False 
 start = time.time()
 
-n=600851475143
-nbr = n 
-L=[]
-while nbr > 1 :
-    i=2
-    while i<=nbr and nbr%i != 0 : 
-        i = i + 1 
-    if nbr%i==0 :
-        nbr = nbr//i 
-        if is_prime(i) :
-            L.append(i) 
+liste=[]
+nbr = 600851475143
+d = 2
+while nbr != 1 :
+	if nbr%d==0 :
+		liste.append(d)
+		nbr=nbr//d
+	else :
+		d = d + 1
+print(liste[-1])
+
 end=time.time() 
 print(L)
 print(f"temps pour trouver la réponse : {end-start}")
